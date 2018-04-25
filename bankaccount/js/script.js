ab44c90a52b0d7020c78006681913fe3ff567473
manuelhernaws <manuelhernaws@gmail.com>
2018-04-25 15:20:08 -0700

    update
     update

--- bankaccount/js/script.js
+++ bankaccount/js/script.js
@@ -1,5 +1,8 @@
 /*eslint-env browser*/
 
+
+// First Way User Name Entry
+/*
 var $ = function (id) {
     "use strict";
     return window.document.getElementById(id);
@@ -58,6 +61,64 @@ window.addEventListener("load", function () {
     });
    
 });
+*/
 
+
+// Second Way No User Name Entry
+
+
+var $ = function (id) {
+    "use strict";
+    return window.document.getElementById(id);
+};
+
+var bankAccount = function (initialBalance) {
+    "use strict";
+    var name = initialBalance,
+        balance = 0,
+        show;
+    show = function (displayMessage) {
+        $("show").innerHTML = displayMessage;
+    };
+    return {
+        cardOwner: function (manny) {
+            name = manny;
+            return name;
+        },
+        getBalance: function () {
+            return balance;
+        },
+        deposit: function (depositAmount) {
+            balance += depositAmount;
+            show(name + " your account balance is " + balance);
+        },
+        withdrawal: function (amount) {
+            if (amount > balance) {
+                window.alert("Your account balance is " + balance + " you must withdrawal withing your balance.");
+            } else {
+                balance -= amount;
+                show(name + " your account balance is " + balance);
+            }
+        }
+    };
+};
+
+window.addEventListener("load", function () {
+    "use strict";
+    var carlosAccount = bankAccount("Carlos Coy");
+    
+    $("deposit").addEventListener("click", function () {
+        var userDeposit = parseInt(window.prompt("How much do you want to deposit"), 10);
+        carlosAccount.deposit(userDeposit);
+        
+    });
+    
+    $("withdrawal").addEventListener("click", function () {
+        var userWithdrawal = parseInt(window.prompt("How much do you want to withdrawal"), 10);
+        carlosAccount.withdrawal(userWithdrawal);
+        
+    });
+   
+});
 
 //https://lazamar.github.io/closures-private-variables-and-methods-in-javascript/
